e717bcde78c786cb6d7933d9f6ef0989495f55a1
Robzlegz <robzlegz@gmail.com>
2021-02-04 19:43:14 +0200

    setting up chart js

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import "./LineGraph.css";
-import {Line} from "chart.js";
+import {Line} from "react-chartjs-2";
 
 const LineGraph = () => {
+
+    const data = [{
+        x: 10,
+        y: 20
+    },
+    {
+        x: 15,
+        y: 10
+    }]
+
     return (
         <div className="lineGraph">
-            <Line data={{
-                datasets: [
-                    {
-                        type: "line",
-                        data: {data}
-                    }
-                ]
-            }} />
+            <Line 
+                data={{
+                    datasets: [
+                        {
+                            type: "line",
+                            data: data,
+                            backgroundColor: "#000",
+                            borderColor: "#5AC53B",
+                            borderWidth: 2,
+                            pointBackgroundColor: "rgba(0,0,0,0)",
+                            pointBackgroundColor: "rgba(0,0,0,0)",
+                            pointHoverBackgroundColor: "#5AC53B",
+                            pointHoverBorderColor: "#000",
+                            pointHoverBorderWidth: 4,
+                            pointHoverRadius: "6",
+                        }
+                    ]
+                }} 
+            />
         </div>
     );
 }
