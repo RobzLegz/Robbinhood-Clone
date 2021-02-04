edb72d180855157ee9cd8b1d842661c89ce71e48
Robzlegz <robzlegz@gmail.com>
2021-02-04 17:09:31 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="headerContainer">
+
+      </div>
+      <div className="bodyContainer">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
