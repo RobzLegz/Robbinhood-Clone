116f2d7cd14db6f3f2afac4a145711236d4ba684
Robzlegz <robzlegz@gmail.com>
2021-02-04 17:44:34 +0200

    creating components

--- src/NewsFeed.js
+++ src/NewsFeed.js
@@ -5,7 +5,12 @@ const NewsFeed = () => {
     return (
         <div className="newsFeed">
             <div className="newsFeedContainer">
-                
+                <div className="newsFeedChartSection">
+                    <div className="portfolioValue">
+                        <h1>$114,656</h1>
+                        <p>+$44.63 (+0.4%) Today</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
