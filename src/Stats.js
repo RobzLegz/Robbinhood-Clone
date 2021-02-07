e357b79f9fddd51da05866a83f41f72a858b4078
Robzlegz <robzlegz@gmail.com>
2021-02-07 18:35:54 +0200

    setting up axios

--- src/Stats.js
+++ src/Stats.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import "./Stats.css"
 
 const Stats = () => {
+
+    const [stockData, setStockData] = useState([]);
+    const BASE_URL = "https://finnhub.io/api/v1/quote";
+    const TOKEN = "c0g15mf48v6rp6j6f9g0";
+
+    const getStockData = (stock) => {
+        return axios.get(`${BASE_URL}?symbol=${stock}&token=${TOKEN}`).catch((error) => {
+            console.log(error);
+        });
+    };
+
+    useEffect(() => {
+        let tempStocksData = [];
+        const stocksList = ["AAPL","MSFT","TSLA","FB","BABA","UBER","DIS","SBUX"];
+        let promises = [];
+        stocksList.map((stock) => {
+            promises.push(
+                getStockData(stock).then((res) => {
+                    tempStocksData.push({
+                        name: stock,
+                        ...res.data,
+                    });
+                })
+            );
+        });
+    }, []);
+
     return (
         <div className="stats">
             <div className="statsContainer">
